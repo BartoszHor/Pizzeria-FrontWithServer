e30621fe93190ae9a4ab9f226adcc13729cfb2d1
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-12-09 13:31:33 +0100

    Add Kitchen view

--- src/components/views/Table/Table.js
+++ src/components/views/Table/Table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Table.module.scss';
 import Table from '@material-ui/core/Table';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,123 +7,131 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TableBody from '@material-ui/core/TableBody';
 import Button from '@material-ui/core/Button';
+import { CgEventbrite } from 'react-icons/cg';
+import { RiReservedFill, RiReservedLine } from 'react-icons/ri';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Link } from 'react-router-dom';
+
+
 
 const tableContent = [
   {
     hour: '9:00',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '1' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '9:30',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '2' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   
   {
     hour: '10:00',
     tables: [
-      { id: 1, status: 'booked' },
-      { id: 2, status: 'booked' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'booked', bookingId: '3' },
+      { id: 2, status: 'booked', bookingId: '4' },
+      { id: 3, status: 'booked', bookingId: '5' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '10:30',
     tables: [
-      { id: 1, status: 'event' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'free' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'event', bookingId: '101' },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'free', bookingId: null },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '11:00',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'event' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'event' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'event', bookingId: '102' },
+      { id: 3, status: 'booked', bookingId: '6' },
+      { id: 4, status: 'event', bookingId: '103' },
     ],
   },
   {
     hour: '11:30',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'booked' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'booked', bookingId: '7' },
+      { id: 3, status: 'booked', bookingId: '8' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '12:00',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '9' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '12:30',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '10' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '13:00',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '11' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '13:30',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '12' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
   {
     hour: '14:00',
     tables: [
-      { id: 1, status: 'free' },
-      { id: 2, status: 'free' },
-      { id: 3, status: 'booked' },
-      { id: 4, status: 'free' },
+      { id: 1, status: 'free', bookingId: null },
+      { id: 2, status: 'free', bookingId: null },
+      { id: 3, status: 'booked', bookingId: '13' },
+      { id: 4, status: 'free', bookingId: null },
     ],
   },
 ];
 
-const renderActions = status => {
+const renderActions = (status, singleTable) => {
+  console.log(singleTable);
   switch (status) {
     case 'free':
       return (
-        <Button>Free</Button>
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/booking/new`}><RiReservedLine/>Free</Button>
       );
     case 'booked':
       return (
-        <Button>Booked</Button>
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/booking/${singleTable.bookingId}`}><RiReservedFill/>Booked</Button>        
       );
     case 'event':
       return (
-        <Button>Event</Button>
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/events/${singleTable.bookingId}`}><CgEventbrite/>Event</Button>
       );
     default:
       return null;
@@ -131,34 +139,49 @@ const renderActions = status => {
 };
 
 const TableView = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
-    <Paper className={styles.component}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Hours</TableCell>
-            <TableCell>Table 1 </TableCell>
-            <TableCell>Table 2 </TableCell> 
-            <TableCell>Table 3 </TableCell> 
-            <TableCell>Table 4 </TableCell>     
-          </TableRow>
-        </TableHead>
-        <TableBody> 
-          {tableContent.map((table) => {
-            return (
-              <TableRow key={table.hour}>
-                <TableCell>{table.hour}</TableCell> 
-                {table.tables.map((singleTable) => {
-                  return (
-                    <TableCell key={singleTable.id}>{renderActions(singleTable.status)}</TableCell>
-                  );
-                })}    
-              </TableRow> 
-            );
-          })} 
-        </TableBody>
-      </Table>
-    </Paper>
+    <div className={styles.component}>
+      <div className={styles.datepicker}>
+        <DatePicker selected={startDate} onChange={date => {
+          return setStartDate(date);
+        }} />
+      </div>
+      <Paper>
+        <Table>
+          <TableHead component='thead'>
+            <TableRow className={styles.tableRow}>
+              <TableCell component='td' className={styles.table}>Hours</TableCell>
+              <TableCell component='td' className={styles.table}>Table 1 </TableCell>
+              <TableCell component='td' className={styles.table}>Table 2 </TableCell> 
+              <TableCell component='td' className={styles.table}>Table 3 </TableCell> 
+              <TableCell component='td' className={styles.table}>Table 4 </TableCell>     
+            </TableRow>
+          </TableHead>
+          <TableBody> 
+            {tableContent.map((table) => {
+              return (
+                <TableRow component='tr' key={table.hour}>
+                  <TableCell className={styles.hour}>{table.hour}</TableCell> 
+                  {table.tables.map((singleTable) => {
+                    return (
+                      <TableCell 
+                        component='tr'
+                        className={styles.link}
+                        to={`${process.env.PUBLIC_URL}/table/booking/${singleTable.bookingId}`}
+                        key={singleTable.id}>{renderActions(singleTable.status, singleTable)}
+                      </TableCell>
+                    );
+                  })}    
+                </TableRow> 
+              );
+            })} 
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+    
   );
 };
 
